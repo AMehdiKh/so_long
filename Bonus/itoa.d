c9c5bb380272dde886b14ff85652dbd0e2d4c1e0
Ahmed El Mehdi El Khelloufi <amehdikh@pop-os.localdomain>
2023-02-13 12:52:19 +0100

    khf

--- Bonus/itoa.d
+++
@@ -1,4 +0,0 @@
-Bonus/itoa.o: Bonus/itoa.c Bonus/so_long.h \
- Bonus/../MLX42/include/MLX42/MLX42.h \
- Bonus/../MLX42/include/MLX42/MLX42_Input.h \
- Bonus/../ft_printf/ft_printf.h Bonus/../LibFT/libft.h
