469eb4686b703790db6a1732e99e4e5455b08779
Ahmed El Mehdi El Khelloufi <amehdikh@pop-os.localdomain>
2023-02-13 12:51:48 +0100

    kh

---
+++ Bonus/sprites.d
@@ -0,0 +1,4 @@
+Bonus/sprites.o: Bonus/sprites.c Bonus/so_long.h \
+ Bonus/../MLX42/include/MLX42/MLX42.h \
+ Bonus/../MLX42/include/MLX42/MLX42_Input.h \
+ Bonus/../ft_printf/ft_printf.h Bonus/../LibFT/libft.h
